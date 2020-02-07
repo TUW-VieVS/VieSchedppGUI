7213083cb0a2c8736a8f855344adc8546441ce0e
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2020-02-07 12:36:30 +0100

    quality of live improvements - single step value of spin boxes changed

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
     int result = 0;
 
     do{
+        QApplication::setStyle("plastique");
         QApplication a(argc, argv);
         MainWindow w;
         w.show();
