04ffae27ad567b8fd4f0521a1e4833a3eff936d4
mschartner <mschartner@ethz.ch>
2023-03-15 17:31:47 +0100

    smaller changes to satellite avoidance GUI

--- Widgets/satelliteavoidancewidget.cpp
+++ Widgets/satelliteavoidancewidget.cpp
@@ -21,6 +21,10 @@ boost::property_tree::ptree SatelliteAvoidanceWidget::toXML()
     double orbitErrorPerDay = ui->spinBox_orbitErrorPerDay->value();
     double checkFrequency =ui->spinBox_checkFrequency->value();
     double minElevation =ui->doubleSpinBox_minElevation->value();
+    double outputPercentage = ui->doubleSpinBox_out->value();
+    if (ui->checkBox_out->isChecked()){
+        outputPercentage = 0;
+    }
 
     boost::property_tree::ptree rules;
     rules.add("satelliteAvoidance.extraMargin", extraMargin);
@@ -28,6 +32,7 @@ boost::property_tree::ptree SatelliteAvoidanceWidget::toXML()
     rules.add("satelliteAvoidance.orbitErrorPerDay", orbitErrorPerDay);
     rules.add("satelliteAvoidance.checkFrequency", checkFrequency);
     rules.add("satelliteAvoidance.minElevation", minElevation);
+    rules.add("satelliteAvoidance.outputPercentage", outputPercentage);
 
     return rules;
 }
@@ -39,12 +44,19 @@ void SatelliteAvoidanceWidget::fromXML(const boost::property_tree::ptree &tree)
     int orbitErrorPerDay = tree.get( "orbitErrorPerDay", 2000 );
     int checkFrequency = tree.get( "checkFrequency", 30 );
     double minElevation = tree.get( "minElevation", 20 );
+    double outputPercentage = tree.get("outputPercentage",0.0);
 
     ui->doubleSpinBox_errorMargin->setValue(extraMargin);
     ui->spinBox_orbitError->setValue(orbitError);
     ui->spinBox_orbitErrorPerDay->setValue(orbitErrorPerDay);
     ui->spinBox_checkFrequency->setValue(checkFrequency);
     ui->doubleSpinBox_minElevation->setValue(minElevation);
+    ui->doubleSpinBox_out->setValue(outputPercentage);
+    if ( outputPercentage > 0){
+        ui->checkBox_out->setChecked(true);
+    }else{
+        ui->checkBox_out->setChecked(false);
+    }
 }
 
 void SatelliteAvoidanceWidget::on_pushButton_save_clicked()
@@ -57,6 +69,7 @@ void SatelliteAvoidanceWidget::on_pushButton_save_clicked()
          << "settings.satelliteAvoidance.orbitErrorPerDay"
          << "settings.satelliteAvoidance.checkFrequency"
          << "settings.satelliteAvoidance.minElevation"
+         << "settings.satelliteAvoidance.outputPercentage"
             ;
 
     value << QString::number(ui->doubleSpinBox_errorMargin->value())
@@ -64,6 +77,7 @@ void SatelliteAvoidanceWidget::on_pushButton_save_clicked()
           << QString::number(ui->spinBox_orbitErrorPerDay->value())
           << QString::number(ui->spinBox_checkFrequency->value())
           << QString::number(ui->doubleSpinBox_minElevation->value())
+          << QString::number(ui->doubleSpinBox_out->value())
              ;
 
     QString name = "satellite avoidance settings";
